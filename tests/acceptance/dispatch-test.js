ac0f35a0eb936a70ff2c259fe035a64e2878cca5
Thomas Chen <tom.chen@am.sony.com>
2016-12-30 23:10:07 -0800

    put in a 15 second idle time for ember to flush its run loop

--- tests/acceptance/dispatch-test.js
+++ tests/acceptance/dispatch-test.js
@@ -1,6 +1,11 @@
+import Ember from 'ember';
 import { test } from 'qunit';
 import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
 import Page from '../../tests/pages/xfire';
+
+function idleFor(time) {
+  return new Ember.RSVP.Promise((resolve) => window.setTimeout(resolve, time));
+}
 
 moduleForAcceptance('Acceptance | dispatch');
 
@@ -11,6 +16,7 @@ test('handling dispatches', function(assert) {
     assert.equal(currentURL(), '/xfire');
     assert.equal(Page.nickname, 'rover');
     Page.writeNickname('edward');
+    return idleFor(15);
   });
 
   andThen(function() {
